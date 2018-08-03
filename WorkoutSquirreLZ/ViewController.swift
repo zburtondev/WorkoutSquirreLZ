0575839b7102e00fe8bcc7d40ddcf6b6e6cd74f8
Zach Burton <zachburton@zachs-macbook-pro.local>
2018-08-02 21:35:36 -0400

    Initial Commit

---
+++ WorkoutSquirreLZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutSquirreLZ
+//
+//  Created by Zach Burton on 8/2/18.
+//  Copyright © 2018 Zach Burton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
